546b54ec4fe4e28d581f30355b38d35230a7e02e
FaysalBearmani <faysalbearmani@yahoo.com>
2017-09-04 20:15:51 +0200

    Update main.js

--- js/main.js
+++ js/main.js
@@ -378,3 +378,9 @@ $(function() {
 		updatePositionFixed();
 	}
 });
+  function send() {
+    var link = 'mailto:email@example.com?subject=Message from '
+             +document.getElementById('email').value
+             +'&body='+document.getElementById('email').value;
+    window.location.href = link;
+}
