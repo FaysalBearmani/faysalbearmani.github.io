208c5bcdf9974358b0f0183c4ef6b21e3364107e
FaysalBearmani <faysalbearmani@yahoo.com>
2017-09-05 21:10:17 +0200

    Update main.js

--- js/main.js
+++ js/main.js
@@ -386,14 +386,11 @@ $(function() {
     **/
 	 // alert('Yoyr message has been sent !');
 	 // sweetAlert("Oops...", "Something went wrong!", "error");
-	  function sendMail() {
-    var link = "mailto:me@example.com"
+              var link = "mailto:me@example.com"
              + "?cc=myCCaddress@example.com"
              + "&subject=" + escape("This is my subject")
-             + "&body=" + escape(document.getElementById('myText').value)
-    ;
+             + "&body=" + escape(document.getElementById('myText').value);
 
     window.location.href = link;
-}
 	  
 }
