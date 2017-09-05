dadc2511b5f3ccfc7179eeeb98b052cd5d4f9657
FaysalBearmani <faysalbearmani@yahoo.com>
2017-09-05 19:38:21 +0200

    Update main.js

--- js/main.js
+++ js/main.js
@@ -384,6 +384,7 @@ $(function() {
              +'&body='+document.getElementById('email').value;
     window.location.href = link; 
     **/
-	  alert('Yoyr message has been sent !');
+	 // alert('Yoyr message has been sent !');
+	  swal("Good job!", "You clicked the button!", "success")
 	  
 }
