21fb8b296d9776ec0d461da11045d527d08ca4cb
FaysalBearmani <faysalbearmani@yahoo.com>
2017-09-05 18:59:23 +0200

    Update main.js

--- js/main.js
+++ js/main.js
@@ -379,8 +379,10 @@ $(function() {
 	}
 });
   function send() {
-    var link = 'mailto:email@example.com?subject=Message from '
+   /** var link = 'mailto:email@example.com?subject=Message from '
              +document.getElementById('email').value
              +'&body='+document.getElementById('email').value;
-    window.location.href = link;
+    window.location.href = link; 
+    **/
+	  alert('yoo');
 }
