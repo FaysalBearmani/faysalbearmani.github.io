a8e5e0a33071346a00de1a594f6ee9c3a5faf03e
FaysalBearmani <faysalbearmani@yahoo.com>
2018-04-24 19:31:52 +0200

    Add files via upload

---
+++ js/dialogJ.js
@@ -0,0 +1,19 @@
+$().ready(
+    $('#fofo').dialog({
+        autoOpen: false,
+        draggable: false,
+        width:800,
+        height:200,
+        hide: { effect: "explode", duration: 1000 },
+
+    }),
+    $( '#fofo' ).dialog( "option", "title", "Nombre d'heures" )
+);
+$('#title').click(function(){
+        $("#fofo").dialog( "open" );
+    }
+);
+$( "#fofo" ).animate({
+    color: "gray",
+    backgroundColor: "rgb(214, 214, 194)"
+});
